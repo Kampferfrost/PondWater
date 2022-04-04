69b5d7c2b95b4c1ab29a218aff25589e5bef5916
Kampferfrost <kampferfrost@gmail.com>
2022-04-04 14:11:03 +0300

    А

---
+++ PondWater/Program.cs
@@ -0,0 +1,12 @@
+﻿//И так я создал проект можно теперь спать
+using System;
+namespace PondWater
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+
+        }
+    }
+}
