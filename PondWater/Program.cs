b1cbcc50240a8703abc70ca0c69590df8bb4fd98
Kampferfrost <kampferfrost@gmail.com>
2022-04-18 22:10:49 +0300

    Проверка на папку, и поиск фалов txt

--- PondWater/Program.cs
+++ PondWater/Program.cs
@@ -1,12 +1,25 @@
-﻿//И так я создал проект можно теперь спать
-using System;
-namespace PondWater
+﻿namespace PondWater
 {
     class Program
     {
         static void Main(string[] args )
         {
+            string dirIn = "In\\"; // папка входящих файлов
+            DirectoryInfo directoryIn = new DirectoryInfo(dirIn);
+            if (directoryIn.Exists)
+            {
+                Console.WriteLine("Каталог существует\n");
 
+                FileInfo[] dirs = directoryIn.GetFiles("*.txt");
+                foreach (FileInfo dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Каталог не найден");
+            }    
         }
     }
 }
