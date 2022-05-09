bfbf7beca05789d2f75014eb24214df1d16fda65
Kampferfrost <kampferfrost@gmail.com>
2022-05-09 07:35:54 +0300

    Переместил в класс ReadFiles

--- PondWater/Program.cs
+++ PondWater/Program.cs
@@ -2,24 +2,14 @@
 {
     class Program
     {
-        static void Main(string[] args )
+        static void Main(string[] args)
         {
-            string dirIn = "In\\"; // папка входящих файлов
-            DirectoryInfo directoryIn = new DirectoryInfo(dirIn);
-            if (directoryIn.Exists)
-            {
-                Console.WriteLine("Каталог существует\n");
 
-                FileInfo[] dirs = directoryIn.GetFiles("*.txt");
-                foreach (FileInfo dir in dirs)
-                {
-                    Console.WriteLine(dir);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Каталог не найден");
-            }    
+            ReadFiles readFiles = new ReadFiles();
+            readFiles.CheckDirAndFiles();    
+
         }
     }
+
+
 }
---
+++ PondWater/ReadFiles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PondWater
+{
+    internal class ReadFiles
+    {
+        internal string DirIn { get; set; } = "In"; // папка входящих файлов
+
+        internal void CheckDirAndFiles()
+        {
+            DirectoryInfo directoryIn = new DirectoryInfo(DirIn);
+
+            if (directoryIn.Exists)
+            {
+                FileInfo[] dirs = directoryIn.GetFiles("*.txt");
+                foreach (FileInfo dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+            }
+            else
+            {
+                directoryIn.Create();
+            }
+
+        }
+
+    }
+
+
+    
+
+
+
+}
